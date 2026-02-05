047c12170c8d5c4c304dc13e1afbb6cbd7ec2751
Nam Le <lehoangnamtep@gmail.com>
2026-02-05 21:13:44 +0000

    Fill bucket automatically with data on first deployement

--- infrastructure/scripts/setup-bucket.sh
+++ infrastructure/scripts/setup-bucket.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ -f /home/init_done.marker ]; then
+    echo "MinIO buckets already initialized. Skipping."
+    exit 0
+fi
+
 sleep 1;
 /usr/bin/mc alias set dockerminio http://minio:9000 root beak-lamp-blind;
 /usr/bin/mc mb dockerminio/test;
-sizes="4KB 8KB 16KB 32KB 64KB 128KB 256KB 512KB 1MB 2MB 4MB 8MB 16MB 32MB 64MB 128MB 256MB 512MB 1GB 2GB 4GB 8GB"
+sizes="4KiB 8KiB 16KiB 32KiB 64KiB 128KiB 256KiB 512KiB 1MiB 2MiB 4MiB 8MiB 16MiB 32MiB 64MiB 128MiB 256MiB 512MiB 1GiB 2GiB 4GiB"
 
 for size in $sizes; do
     filename="file-${size}.dat"
@@ -11,3 +16,5 @@ for size in $sizes; do
     mc cp "/tmp/${filename}" dockerminio/test/
     rm "/tmp/${filename}"    
 done
+
+touch /home/init_done.marker
